7e0ef3673d3e3f0575cf615839f1f31765560224
Elliot Blanchard <public@elliotblanchard.com>
2020-04-07 01:59:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -8,3 +8,44 @@ WIN_COMBINATIONS = [
   [0,4,8], # Diag A
   [6,4,2], # Diag B
 ]
+
+#display_board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board,index,character)
+  board[index] = character
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+#valid_move?
+def valid_move?(board,index)
+  if ( index.between?(0,8) && position_taken?(board,index) == false)
+    return true
+  end
+end
+
+#turn
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board,index)
+    move(board,index,character="X")
+    display_board(board)
+  else turn(board)
+  end
+end
