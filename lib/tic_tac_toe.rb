a37d5e482d8cae65b6590ab9c1a7f54ba3d2bb2e
Elliot Blanchard <public@elliotblanchard.com>
2020-04-07 01:50:46 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,39 @@
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Top column
+  [1,4,7], # Middle column
+  [2,5,8], # Bottom column
+  [0,4,8], # Diag A
+  [6,4,2], # Diag B
+]
+
+#display_board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board,index,character)
+  board[index] = character
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
+#valid_move?
+def valid_move?(board,index)
+  if ( index.between?(0,8) && position_taken?(board,index) == false)
+    return true
+  end
+end
