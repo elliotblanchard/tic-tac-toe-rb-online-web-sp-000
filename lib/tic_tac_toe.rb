519ed772f78a9b45a2cd1f8a738432968ec3d38d
Elliot Blanchard <public@elliotblanchard.com>
2020-04-07 01:57:35 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,3 +59,12 @@ def turn_count(board)
   end
   return counter
 end
+
+#current_player
+def current_player(board)
+  if turn_count(board) % 2 = 0
+    return "X"
+  else
+    return "O"
+  end
+end
